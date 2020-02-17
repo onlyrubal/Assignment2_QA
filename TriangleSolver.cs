bda9f0ad1ced7f72959e4f8e3a9c3389ccc4fc69
Manjinder Singh <Manjindersingh8670@conestogac.on.ca>
2020-02-16 23:07:33 -0500

    Initialized static class TriangleSolver and it's static method Analyze

---
+++ TriangleSolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Assignment2_QA
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int side1, int side2, int side3)
+        {
+        }
+
+    }
+}
